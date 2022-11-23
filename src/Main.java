3a0778df6bd7b446b2b1d328e691f589df27f147
Dima <dmitriy.matveev89@inbox.ru>
2022-11-23 13:25:17 +0300

    Ver 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 /*
@@ -7,11 +10,58 @@ public class Main {
          –азработайте список дел, который управл€етс€ командами в консоли.  оманды:
          LIST, ADD, EDIT, DELETE.
          ѕринцип работы команд:
-         
+
 Х	LIST Ч выводит дела с их пор€дковыми номерами;
 Х	ADD Ч добавл€ет дело в конец списка или дело на определЄнное место, сдвига€ остальные дела вперЄд, если указать номер;
 Х	EDIT Ч замен€ет дело с указанным номером;
 Х	DELETE Ч удал€ет.
 */
+        final String LC_LIST = "LIST";
+        final String LC_ADD = "ADD";
+        final String LC_EDIT = "EDIT";
+        final String LC_DELETE = "DELETE";
+
+        Scanner loScanner = new Scanner(System.in);
+        String lvTextBlock = """
+                ¬ведите команду из списка:  
+                         LIST
+                         ADD текст 
+                         ADD пор€дковый номер текст
+                         EDIT пор€дковый номер текст
+                         DELETE пор€дковый номер
+                """;
+        System.out.println(lvTextBlock);
+        String loScannerLine = loScanner.nextLine();
+//        String loScannerLine = "ADD 0  акое-то дело на нулевом месте"; //Test
+
+        ArrayList<String> ltArrayList = new ArrayList<>();
+
+        Teams loTeams = new Teams();
+
+        //ѕоиск первого слова
+        String[] ltWordsFromScanner = loScannerLine.split("\\s+");
+        String lvFirstWordFromScanner = ltWordsFromScanner[0];
+
+        //LIST
+        if (lvFirstWordFromScanner.equals(LC_LIST)) {
+            ltArrayList = loTeams.executeList();
+        }
+        //ADD
+        if (lvFirstWordFromScanner.equals(LC_ADD)) {
+            ltArrayList = loTeams.executeADD(loScannerLine, ltWordsFromScanner);
+        }
+        //EDIT
+        if (lvFirstWordFromScanner.equals(LC_EDIT)) {
+            //«амен€ет дело с указанным номером
+//            ltArrayList = executeEDIT();
+        }
+        //DELETE
+        if (lvFirstWordFromScanner.equals(LC_DELETE)) {
+            //”дал€ет
+//            ltArrayList = executeDELETE();
+        }
+
+        //¬ывод на экран
+        loTeams.showList(ltArrayList);
     }
 }
---
+++ src/Teams.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+
+public class Teams {
+    private final ArrayList<String> mtArrayList = new ArrayList<>();
+
+    /*
+        method executeList
+
+        Выводит дела с их порядковыми номерами
+    */
+    public ArrayList<String> executeList() {
+        for (int i = 0; i < mtArrayList.size(); i++) {
+            System.out.println((i + 1) + mtArrayList.get(i));
+        }
+        return mtArrayList;
+    }
+
+    /*
+        method executeADD
+
+        Добавляет дело в конец списка или дело на определённое место, сдвигая остальные дела вперёд,
+        если указать номер
+    */
+    public ArrayList<String> executeADD(String ioScannerStr,
+                                        String[] itWordsFromScanner) {
+        StringBuilder lvTextAfterAdd = new StringBuilder();
+//        String lvTextAfterAdd = "";
+        int lvPozNumber = 0;
+
+        try {
+            //Проверка на число в строке после первого слова
+            int intValue = Integer.parseInt(itWordsFromScanner[1]);
+
+            //С порядковым номером
+            for (int i = 1; i < itWordsFromScanner.length; i++) {
+                if (i == 1) {
+                    lvPozNumber = Integer.parseInt(itWordsFromScanner[i]);
+                } else {
+                    lvTextAfterAdd.append(itWordsFromScanner[i]);
+                    lvTextAfterAdd.append(" ");
+//                    lvTextAfterAdd += itWordsFromScanner[i] + " ";
+                }
+            }
+            //Добавить в список с порядковым номером
+            if (lvPozNumber > mtArrayList.size()) {
+                System.out.println("Позиция " + lvPozNumber + " превышает длинну списка " + mtArrayList.size());
+                //Выход из программы
+                System.exit(0);
+            }
+            mtArrayList.add(lvPozNumber, lvTextAfterAdd.toString().trim());
+//            mtArrayList.add(lvPozNumber, lvTextAfterAdd.trim());
+        } catch (NumberFormatException e) {
+            //Без порядкового
+            for (int i = 1; i < itWordsFromScanner.length; i++) {
+                lvTextAfterAdd.append(itWordsFromScanner[i]);
+                lvTextAfterAdd.append(" ");
+//                lvTextAfterAdd += itWordsFromScanner[i] + " ";
+            }
+            //Добавить в список
+            mtArrayList.add(lvTextAfterAdd.toString().trim());
+//            mtArrayList.add(lvTextAfterAdd.trim());
+        }
+        return mtArrayList;
+    }
+
+    public void showList(ArrayList<String> itArrayList) {
+        for (int i = 0; i < itArrayList.size(); i++) {
+            System.out.println(itArrayList.get(i));
+        }
+    }
+}
